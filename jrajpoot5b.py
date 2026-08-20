f9625f4e7bf3b56e45c192899cf19ad5b02a0e88
jitenderrajpoot <jitender.rajpoot@lausd.net>
2026-08-19 18:53:31 -0700

    Print List with Loop

---
+++ jrajpoot5b.py
@@ -0,0 +1,8 @@
+#Jitender Rajpoot
+#Lab5b
+# Create a list with 10 even numbers. Use different numbers than the sample output.
+evenNum = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+
+# Print out each element in the list using a loop.
+for item in evenNum:
+  print(item)
