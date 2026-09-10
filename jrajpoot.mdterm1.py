822c10a0db43b489c441ffcc6e996e6d862d0d2b
Jitender Rajpoot <jitender.rajpoot@lausd.net>
2026-09-10 00:12:29 +0000

    Midterm question1

---
+++ jrajpoot.mdterm1.py
@@ -0,0 +1,16 @@
+#Jitender Rajpoot
+#Midterm Question1
+
+#Ask a user to enter their name.
+name = input("Enter your name: ")
+
+#Ask the user to enter a number.
+number1 = int(input("Enter a number: "))
+
+#Ask the user how much they would like to add to the number.
+number2 = int(input("How much to add to that number?: "))
+total = number1 + number2
+
+#Display the user's name, the number entered, the number that was added, and the total.
+print(name, " you entered ", number1, " and added ", number2, " to get a total of ", total)
+
